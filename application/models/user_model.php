399c52ef9ff486a5d00ea18143342545ae526516
William Pitchard <willp20@hotmail.com>
2018-03-24 22:56:19 +0000

    edit email setting

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -16,21 +16,21 @@ class user_model extends CI_Model
     //send verification email to user's email id
     function sendEmail($to_email)
     {
-        $from_email = 'team@mydomain.com'; //change this to yours
+        $from_email = 'willliam.pritchard@williampritchard.co.uk'; //change this to yours
         $subject = 'Verify Your Email Address';
         $message = 'Dear User,<br /><br />Please click on the below activation link to verify your email address.<br /><br /> http://www.mydomain.com/user/verify/' . md5($to_email) . '<br /><br /><br />Thanks<br />Mydomain Team';
         
         //configure email settings
-        $config['protocol'] = 'smtp';
-        $config['smtp_host'] = 'ssl://smtp.mydomain.com'; //smtp host name
+        /* $config['protocol'] = 'smtp';
+        $config['smtp_host'] = 'ssl://smtp.williampritchard.co.uk'; //smtp host name
         $config['smtp_port'] = '465'; //smtp port number
         $config['smtp_user'] = $from_email;
-        $config['smtp_pass'] = '********'; //$from_email password
+        $config['smtp_pass'] = 'burton83'; //$from_email password
         $config['mailtype'] = 'html';
         $config['charset'] = 'iso-8859-1';
         $config['wordwrap'] = TRUE;
         $config['newline'] = "\r\n"; //use double quotes
-        $this->email->initialize($config);
+        $this->email->initialize($config); */
         
         //send mail
         $this->email->from($from_email, 'Mydomain');
