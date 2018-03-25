014428d2e172055208dc8d88e71b50625c307171
William Pitchard <willp20@hotmail.com>
2018-03-25 15:08:33 +0100

    made jquery functional andd tidy

--- js/homepage.js
+++ js/homepage.js
@@ -1,41 +1,50 @@
-
-$(document).ready(function () {
- $( "#target" ).submit(function( event ) {
-	 data = $(this).serialize();
+var blu = {
 	
-	  $.ajax({
-			 type: 'POST',
-			 url: 'http://localhost/blubolt_demo/index.php/user/register',
-			dataType: "json",
-			data: data,
-			success: function(resp) { 
-					
-					if (resp.error==1){
-						
-						$("#errors").html(resp.result);
-					}
-					else{
-					
-						$("#name").text(resp.result.name);
-						$("#email").text(resp.result.email);
-						$("#comment").text(resp.result.comment);
-						$( "#myModal" ).modal('show');	
-					}
-					
-					
-				   //$("#mycart li").show( "slow" );
-				   
-				   //  $("#To").text(resp);
-					//$('select#match_list').attr('disabled',false).html(resp);
-					//With the ".html()" method we include the html code returned by AJAX into the matches list
-				},
-			error: function(jqxHR, except){
-				$("#errors").html('<div class="alert alert-danger text-center">eror</div>');
-			}
+	onReady : function(){
+				$( "#target" ).submit(blu.onSubmit); 
+		
+	} ,
+	onSubmit: function( event ) {
+			 data = $(this).serialize();
+			
+			  $.ajax({
+					 type: 'POST',
+					 url: 'http://localhost/blubolt_demo/index.php/user/register',
+					dataType: "json",
+					data: data,
+					success: blu.succ,
+					error: blu.err,
 
-				});
-	
- //alert( "Handler for .submit() called." );
-  event.preventDefault();
-	}); 
-});
+						});
+			
+		 //alert( "Handler for .submit() called." );
+		  event.preventDefault();
+	},
+	succ: function(resp) { 
+							
+							if (resp.error==1){
+								
+								$("#errors").html(resp.result);
+							}
+							else{
+							
+								$("#name").text(resp.result.name);
+								$("#email").text(resp.result.email);
+								$("#comment").text(resp.result.comment);
+								$( "#myModal" ).modal('show');	
+							}
+							
+							
+						   //$("#mycart li").show( "slow" );
+						   
+						   //  $("#To").text(resp);
+							//$('select#match_list').attr('disabled',false).html(resp);
+							//With the ".html()" method we include the html code returned by AJAX into the matches list
+						},
+	err :function(jqxHR, except){
+						$("#errors").html('<div class="alert alert-danger text-center">eror</div>');
+					}
+};
+$(document).ready(blu.onReady);
+
+
