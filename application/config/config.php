99167dbb91fb58f169fda0f1877749c1cd4d8f4a
William Pitchard <willp20@hotmail.com>
2018-05-21 11:43:53 +0100

    change base url
    hello_future

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/blubolt_demo/';
+$config['base_url']	= 'http://localhost/hello_future/';
 
 /*
 |--------------------------------------------------------------------------
